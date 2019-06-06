05a263b5e11451579ca136917ab2cb9865782283
Aritra Ghosh <aritra119.26@gmail.com>
2019-06-06 17:28:53 +0530

    Renamed some files, checked for missing resource key and made request parser (#3)

--- handlers/adapter.go
+++ handlers/adapter.go
@@ -1,38 +1,34 @@
 package handlers
 
 import (
-	"encoding/json"
 	"go-ms-adapter/config"
+	"go-ms-adapter/constants"
 	"go-ms-adapter/services"
 	"go-ms-adapter/utils"
-	"go-ms-adapter/constants"
-	"io/ioutil"
 	"net/http"
+	"reflect"
 )
 
 // GenericHandler : Resource key based handler
 func GenericHandler(w http.ResponseWriter, r *http.Request) {
-	bodyBuffer, _ := ioutil.ReadAll(r.Body)
-
-	//request
-	var result map[string]interface{}
-	json.Unmarshal([]byte(string(bodyBuffer)), &result)
-	resourceKey := result["resourceKey"].(string)
-	delete(result, "resourceKey")
-
-	isRequestValid, errMessage := services.RequestValidator(config.Configs[resourceKey].RequestParams, result)
+	requestPayload, resourceKey := utils.ParseRequest(r)
+	if reflect.DeepEqual(config.Configs[resourceKey], config.TemplateParams{}) {
+		utils.Error(w, http.StatusBadRequest, "Resource key not found")
+		return
+	}
+	isRequestValid, errMessage := services.RequestValidator(config.Configs[resourceKey].RequestParams, requestPayload)
 	if !isRequestValid {
 		utils.Error(w, http.StatusBadRequest, errMessage)
 		return
 	}
+
 	requestTemplateName := config.Configs[resourceKey].RequestTemplate
-	method := config.Configs[resourceKey].Method
 	requestTemplateConfig := config.Templates[requestTemplateName]
-
+	method := config.Configs[resourceKey].Method
 	queryParams := config.Configs[resourceKey].QueryParams
-	requestTemplate := services.FormRequest(requestTemplateConfig, result)
+	url := config.Configs[resourceKey].URL + services.FormQueryParams(method, requestPayload, queryParams)
 
-	url := config.Configs[resourceKey].URL + services.FormQueryParams(method, result, queryParams)
+	requestTemplate := services.FormRequest(requestTemplateConfig, requestPayload)
 	out := services.MakeRemoteRequest(url, method, requestTemplate)
 
 	if _, ok := constants.ERROR_MESSAGES[out]; ok {

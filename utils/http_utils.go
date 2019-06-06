20431e9d0d3b0549e41ed1d6eb67a3eae032880c
Aritra Ghosh <aritra119.26@gmail.com>
2019-06-06 17:25:04 +0530

    Renamed some files, checked for missing resource key and made request parser

--- utils/http_utils.go
+++ utils/http_utils.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -17,4 +18,14 @@ func JSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	w.Write(response)
+}
+
+// ParseRequest - parse http request
+func ParseRequest(r *http.Request) (map[string]interface{}, string) {
+	bodyBuffer, _ := ioutil.ReadAll(r.Body)
+	var result map[string]interface{}
+	json.Unmarshal([]byte(string(bodyBuffer)), &result)
+	resourceKey := result["resourceKey"].(string)
+	delete(result, "resourceKey")
+	return result, resourceKey
 }
